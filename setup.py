27127b2c59adf517d60678c567550d2002b00162
sunrize <sunrize@wysegames.com>
2012-12-17 21:59:40 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='cherry-forms',
+    version='0.1a',
+    packages=['cherrycommon'],
+    url='',
+    license='MIT',
+    author='WYSEGames',
+    author_email='info@wysegames.com',
+    description='Set of UIModules for building forms with Tornado web server. Widgets also can be used as standalone on clientside.',
+    install_requires=[
+        "tornado >= 2.4",
+        "pymongo >= 2.3"
+    ],
+)
