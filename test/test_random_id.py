3aec46bc0bfd999d3e72518e420b867d45ae658d
Ivan Filimonov <i.filimonov@wysegames.com>
2013-02-05 20:46:40 +0200

    Now we have to function for id generation - unique_id and random_id.

--- test/test_random_id.py
+++ test/test_random_id.py
@@ -1,13 +1,13 @@
-from cherrycommon.mathutils import random_id
+from cherrycommon.mathutils import random_id, unique_id
 
 __author__ = 'sunrize'
 
 import unittest
 
 class RandomIDTest(unittest.TestCase):
-    def test_random_id(self):
-        rid0 = random_id()
-        rid1 = random_id()
+    def test_unique_id(self):
+        rid0 = unique_id()
+        rid1 = unique_id()
         self.assertGreater(rid1, rid0)
 
 if __name__ == '__main__':
