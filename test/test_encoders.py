ff2a8bd7ae123b6e846c7f410c7fe2f9df44e653
Ivan Filimonov <i.filimonov@wysegames.com>
2013-11-08 11:40:18 +0200

    Tests for encoder

--- test/test_encoders.py
+++ test/test_encoders.py
@@ -1,12 +1,33 @@
-from cherrycommon.excel import XLS
+from cherrycommon.dictutils import encode_data, decode_data, AMF, JSON, XJSON, YAML
 import unittest
 
 
-class ExcelTest(unittest.TestCase):
-    def test_cell_names(self):
-        self.assertEqual(XLS.get_cell_name(0, 0), 'A1')
-        self.assertEqual(XLS.get_cell_name(25, 0), 'Z1')
-        self.assertEqual(XLS.get_cell_name(26, 9), 'AA10')
+class EncoderTest(unittest.TestCase):
+    src = {
+        'plain': 1,
+        'nested': {
+            'plain': 1,
+            'list': [1, 2]
+        },
+        'list': [1, 2, 3]
+    }
+
+    def _is_match(self, re):
+        self.assertEquals(re['plain'], self.src['plain'])
+        self.assertEquals(re['nested'], self.src['nested'])
+        self.assertEquals(re['list'].index(1), 0)
+
+    def test_amf(self):
+        re = decode_data(encode_data(self.src, AMF), AMF)
+        self._is_match(re)
+
+    def test_json(self):
+        re = decode_data(encode_data(self.src, JSON), JSON)
+        self._is_match(re)
+
+    def test_xjson(self):
+        re = decode_data(encode_data(self.src, XJSON), XJSON)
+        self._is_match(re)
 
 
 if __name__ == '__main__':
